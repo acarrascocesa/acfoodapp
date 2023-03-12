4f11d6d6a1ddab465bf3e80e4d5a81962a738fb1
Angel Carrasco <acarrascocesa@gmail.com>
2023-03-12 10:59:03 -0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { SidebarProvider } from "./context/sidebarContext";
+import { MealProvider } from "./context/mealContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <SidebarProvider>
+    <MealProvider>
+      <App />
+    </MealProvider>
+  </SidebarProvider>
+);
